e970c9737ea53101e1cdfe051e4042ea511db95d
nerdalytics <97166791+nerdalytics@users.noreply.github.com>
2026-05-17 20:01:03 +0200

    feat(app): wire ViviraDesignSystem and render NotConnected empty state

--- Vivira/ViviraApp.swift
+++ Vivira/ViviraApp.swift
@@ -1,10 +1,12 @@
 import SwiftUI
+import ViviraDesignSystem
 
 @main
 struct ViviraApp: App {
     var body: some Scene {
         WindowGroup {
             ContentView()
+                .environment(\.viviraTheme, .lumen)
         }
     }
 }
