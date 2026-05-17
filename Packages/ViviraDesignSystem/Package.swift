2b4b4aad74d5a2cb7d1b7f50c4bcdb9681b2d9fe
nerdalytics <97166791+nerdalytics@users.noreply.github.com>
2026-05-17 22:17:58 +0200

    build(ds): add test target with swift-snapshot-testing dep

--- Packages/ViviraDesignSystem/Package.swift
+++ Packages/ViviraDesignSystem/Package.swift
@@ -10,10 +10,20 @@ let package = Package(
     products: [
         .library(name: "ViviraDesignSystem", targets: ["ViviraDesignSystem"])
     ],
+    dependencies: [
+        .package(url: "https://github.com/pointfreeco/swift-snapshot-testing", from: "1.17.0")
+    ],
     targets: [
         .target(
             name: "ViviraDesignSystem",
             resources: [.process("Resources/ViviraColors.xcassets")]
+        ),
+        .testTarget(
+            name: "ViviraDesignSystemTests",
+            dependencies: [
+                "ViviraDesignSystem",
+                .product(name: "SnapshotTesting", package: "swift-snapshot-testing")
+            ]
         )
     ]
 )
